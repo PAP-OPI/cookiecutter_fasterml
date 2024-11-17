a091f33d3ef3347f25a29f1758c701562ff9745e
Rafael Gallardo <rafaelgallardo2836@gmail.com>
2024-11-17 17:14:29 -0600

    added API and format with ruff

--- {{cookiecutter.repo_name}}/src/API.py
+++ {{cookiecutter.repo_name}}/src/API.py
@@ -1,9 +1,10 @@
 import time
-from starlette.responses import Response
+
 from fastapi import FastAPI, Request
 from prometheus_client import Counter, Histogram, generate_latest
+from starlette.responses import Response
 
-app:FastAPI = FastAPI()
+app: FastAPI = FastAPI()
 
 REQUEST_COUNT = Counter(
     "request_count", "App Request Count", ["method", "endpoint", "http_status"]
@@ -12,23 +13,28 @@ REQUEST_LATENCY = Histogram(
     "request_latency_seconds", "Request latency in seconds", ["endpoint"]
 )
 
+
 @app.middleware("http")
 async def add_prometheus_metrics(request: Request, call_next):
     """
     Middleware to collect Prometheus metrics for request counts and latency.
     """
-    start_time:float = time.time()
+    start_time: float = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
 
     global REQUEST_COUNT
     REQUEST_COUNT.labels(
-        method=request.method, endpoint=request.url.path, http_status=response.status_code).inc()
-    
+        method=request.method,
+        endpoint=request.url.path,
+        http_status=response.status_code,
+    ).inc()
+
     global REQUEST_LATENCY
     REQUEST_LATENCY.labels(endpoint=request.url.path).observe(process_time)
 
     return response
+
 
 @app.get("/health")
 async def health_check():
@@ -37,12 +43,14 @@ async def health_check():
     """
     return {"status": "Todo Gucci"}
 
+
 @app.get("/metrics")
 async def metrics():
     """
     Expose Prometheus metrics at the /metrics endpoint.
     """
     return Response(generate_latest(), media_type="text/plain")
+
 
 @app.post("/predict")
 async def predict(data: dict):
@@ -57,5 +65,3 @@ async def predict(data: dict):
     prediction = {"result": "default_prediction"}
 
     return prediction
-
-
