bde785d1bbb2da0a61d57bdeb74c5cc99eba5336
Rafael Gallardo <rafaelgallardo2836@gmail.com>
2024-11-18 00:34:38 -0600

    created executable file for CLI

--- {{cookiecutter.model_name}}_fasterml/src/exec.py
+++ {{cookiecutter.model_name}}_fasterml/src/exec.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     x = sqlite.populate_class(yaml_file)
     sqlite.populate_sqlite(x.database_config)
     lr = {{cookiecutter.model_name}}()
-    data_train = pd.read_csv("./data/train.csv")
+    data_train = pd.read_csv(x.database_config.path)
     train_data = lr.preprocess_data(data_train)
     lr.train(train_data)
     lr.export()
