8cd0faab68331b4cf1d9934a61503e0a0c939258
Rafael Gallardo <43729882+G4ll4rd0@users.noreply.github.com>
2024-11-23 00:13:56 -0600

    Revert "Revert "Dev v.0.0.1""

---
+++ {{cookiecutter.model_name}}_fasterml/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from src.classes import Trainable
+
+class {{cookiecutter.model_name}}(Trainable):
+    def preprocess_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        preprocessed_data = df.copy()
+
+        # AQUI REALIZA TODO EL PREPROCESAMIENTO DE TUS DATOS
+
+        return preprocessed_data
+
+    def train(self, df: pd.DataFrame) -> any:
+
+        self.model = #Aqui mandas llamar tu modelo (sklearn, statsmodels, etc.)
+        self.model.fit(df['X'], df['y'])
